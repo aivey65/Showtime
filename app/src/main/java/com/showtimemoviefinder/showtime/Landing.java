fd7dcb7f1ede10475e0a3fcff6162c3ddc7520cf
aivey65 <39443120+aivey65@users.noreply.github.com>
2018-12-03 22:45:22 -0600

    Some additions to the layout and functionality, but not much yet.

--- app/src/main/java/com/showtimemoviefinder/showtime/Landing.java
+++ app/src/main/java/com/showtimemoviefinder/showtime/Landing.java
@@ -12,6 +12,31 @@ public class Landing extends AppCompatActivity {
     }
 
 
+    public void processZip(android.view.View view) {
+        //Include the things you want to do with the zip
+        //1. Get the text within the text box "zipcodeInput".
+        //2. Send this number to the API to get a list of movie theaters
+        //3. Display 5 of these movie theaters. Maybe this will be on a different layout.
+    }
 
+
+    /**
+     * below is an example of how the api can be used, but we would use the input rather than a
+     * @param result
+     */
+//    FandangoApiManager api = new FandangoApiManager();
+//    api.delegate = this; // Be sure to implement "FandangoApiResultDelegate" in your activity declaration
+//
+//    String zipCode = "90064";
+//    String parameters = String.format("op=theatersbypostalcodesearch&postalcode=%s", zipCode);
+//
+//        api.execute(parameters);
+
+
+
+    public void gotResult(String result) {
+
+        // Process response data...
+    }
 
 }
